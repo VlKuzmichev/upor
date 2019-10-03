c9c1b9adff26c39318b75178e21010183a925136
KuzmichevVB <echo_vk1@yahoo.com>
2019-10-03 10:33:10 +0900

    Fix Encoding(add filter)

---
+++ src/test/java/rzd/zrw/upor/web/json/JsonUtilTest.java
@@ -0,0 +1,25 @@
+package rzd.zrw.upor.web.json;
+
+import org.junit.jupiter.api.Test;
+import rzd.zrw.upor.UserTestData;
+import rzd.zrw.upor.model.User;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JsonUtilTest {
+
+    @Test
+    void testWriteOnlyAccess() throws Exception {
+        String json = JsonUtil.writeValue(UserTestData.USER);
+        System.out.println(json);
+        assertThat(json, not(containsString("password")));
+        String jsonWithPass = UserTestData.jsonWithPassword(UserTestData.USER, "newPass");
+        System.out.println(jsonWithPass);
+        User user = JsonUtil.readValue(jsonWithPass, User.class);
+        assertEquals(user.getPassword(), "newPass");
+    }
+
+}
